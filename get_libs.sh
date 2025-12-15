1b4ea9a6bbe2adedaa1544ac9ee36976519de81a
Juhani Kataja <juhani.kataja@csc.fi>
2025-12-15 13:30:21 +0200

    Fixes on configure_socol_lumi and get_libs.sh
    - Don't force MPI libs flag for MPIOM but rely on wrapper to do that
    - Fetch yaxt insecurely because swprojects.dkrz.de certificate expired Oct
      2025

--- get_libs.sh
+++ get_libs.sh
@@ -7,7 +7,7 @@ curl -LO https://code.mpimet.mpg.de/attachments/download/28013/cdo-2.2.0.tar.gz 
 curl -LO https://gitlab.dkrz.de/mpim-sw/libcdi/-/archive/cdi-1.7.2/libcdi-cdi-1.7.2.tar.gz && \
   tar xfvz libcdi-cdi-1.7.2.tar.gz  
 
-curl -LO https://swprojects.dkrz.de/redmine/attachments/download/506/yaxt-0.9.1.tar.gz && \
+curl --insecure -LO https://swprojects.dkrz.de/redmine/attachments/download/506/yaxt-0.9.1.tar.gz && \
   tar xfvz yaxt-0.9.1.tar.gz 
 
 popd
