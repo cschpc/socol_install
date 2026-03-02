6db2b1642323d556af8b6ef1f6c228ee0d576f85
Juhani Kataja <juhani.kataja@csc.fi>
2026-03-02 10:46:59 +0200

    Don't get cdo-2.2.0 from external source

--- get_libs.sh
+++ get_libs.sh
@@ -1,8 +1,8 @@
 mkdir -p downloads
 pushd downloads 
 
-curl -LO https://code.mpimet.mpg.de/attachments/download/28013/cdo-2.2.0.tar.gz && \
-  tar xfvz cdo-2.2.0.tar.gz && \
+#curl -LO https://code.mpimet.mpg.de/attachments/download/28013/cdo-2.2.0.tar.gz && \
+#  tar xfvz cdo-2.2.0.tar.gz && \
 
 curl -LO https://gitlab.dkrz.de/mpim-sw/libcdi/-/archive/cdi-1.7.2/libcdi-cdi-1.7.2.tar.gz && \
   tar xfvz libcdi-cdi-1.7.2.tar.gz  
